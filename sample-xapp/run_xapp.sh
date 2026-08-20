08450e586f1fbd80b28e3a8103d28af65c7a792c
YIZHOU WANG <yichow.wang@gmail.com>
2026-08-20 14:59:56 +0200

    Fix script to run the correct xApp Python file

--- sample-xapp/run_xapp.sh
+++ sample-xapp/run_xapp.sh
@@ -5,8 +5,8 @@ kill -9 `pidof python3`
 
 # Run agent, sleep, run connector
 echo "[`date`] Run xApp" > /home/container.log
-# cd /home/sample-xapp && python3 run_xapp.py &
-cd /home/sample-xapp && python3 xApp_conflict/run_xapp.py &
+cd /home/sample-xapp && python3 run_xapp.py &
+
 
 echo "[`date`] Pause 1 s" >> /home/container.log
 sleep 1
